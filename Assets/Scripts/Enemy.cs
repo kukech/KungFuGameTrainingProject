5ba0e3937cc92c4c0d4859d1627b0d52573f2bdf
kukech <qwadik@gmail.com>
2021-11-04 20:16:24 +0300

    нашёл ресурсы, сделал анимацию игрока и врага, враг движется к игроку.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] public PlayerMain _player;
+    [SerializeField] public float speed;
+    [SerializeField] public int hp;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = _player.transform.position - transform.position;
+        movement = Vector3.ClampMagnitude(new Vector3(movement.x, movement.y, 0), speed) * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour
+{
+    private Rigidbody2D _body;
+    private Animator _anim;
+    public float impulsePower;
+    private float deltaX;
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_body.velocity.x == 0 && Input.GetAxis("Horizontal") != 0)
+        {
+            deltaX = Mathf.Sign(Input.GetAxis("Horizontal")) * impulsePower /*Time.deltaTime **/;
+            Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+            transform.localScale = new Vector3(Mathf.Sign(deltaX)*Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            _body.velocity = movement;
+            _anim.SetBool("IsAttack", true);
+            StartCoroutine(Force());
+        }
+    }
+    public IEnumerator Force()
+    {
+        yield return new WaitForSeconds(0.7f);
+        _body.velocity = Vector3.zero;
+        _anim.SetBool("IsAttack", false);
+    }
+}
